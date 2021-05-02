f92334fe6d9d420460c7d1a73d34847eab1e839e
Juejie Pan <panapril77@gmail.com>
2021-05-02 15:16:04 -0400

    upload image to create food

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
-import {API, graphqlOperation} from "aws-amplify";
+import {API, graphqlOperation, Storage} from "aws-amplify";
 import React from 'react';
 import { useHistory } from "react-router";
 import { createJPFood } from "../graphql/mutations";
 import { listJPFoods, listOrders } from "../graphql/queries";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Home() {
     const getfoods = async () => {
@@ -21,6 +22,33 @@ export default function Home() {
     const history = useHistory()
     const handlegotologin = () => history.push('/login')
 
+    const bucket = "jpnst4d38dc0062eb4c7b886149e388b15749215416-dev"
+    const region = "ca-central-1"
+    const [image, setImage] = React.useState(null);
+    const handleImageUpload = async (e) => {
+        e.preventDefault();
+        const file = e.target.files[0];
+        const extension = file.name.split(".")[1];
+        const name = file.name.split(".")[0];
+        const key = `images/${uuidv4()}${name}.${extension}`;
+        const url = `https://${bucket}.s3.${region}.amazonaws.com/public/${key}`
+        console.log(url)
+        try {
+            // Upload the file to s3 with public access level. 
+            await Storage.put(key, file, {
+                level: 'public',
+                contentType: file.type
+            });
+            // Retrieve the uploaded file to display
+            const image = await Storage.get(key, { level: 'public' })
+            setImage(image);
+            setInput({ ...input, image: url });
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     const [input, setInput] = React.useState({ title: "", description: "", image: "", author: "", price: "" });
     const createFood = async () => {
         const data = await API.graphql(graphqlOperation(createJPFood, {input: input}))
@@ -28,6 +56,7 @@ export default function Home() {
     }
     const handleSubmit = (e) => {
         e.preventDefault()
+        console.log(input)
         createFood()
     }
 
@@ -39,6 +68,13 @@ export default function Home() {
             <button onClick={handlegotologin}>go to login</button>
             <form className="form-wrapper" onSubmit={handleSubmit}>
                         <div className="form-fields">
+                        <div className="form-image">
+                            {image ? <img className="image-preview" src={image} alt="" width={300} /> : <input
+                                type="file"
+                                accept="image/jpg"
+                                onChange={(e) => handleImageUpload(e)} />}
+
+                        </div>
                             <div className="title-form">
                                 <p><label htmlFor="title">Title</label></p>
                                 <p><input
