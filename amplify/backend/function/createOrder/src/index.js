f85c0aeb2fa32014ff328621e4152f65ae33447e
Juejie Pan <panapril77@gmail.com>
2021-05-03 19:41:58 -0400

    create order

--- amplify/backend/function/createOrder/src/index.js
+++ amplify/backend/function/createOrder/src/index.js
@@ -1,4 +1,21 @@
-exports.handler = async (event) => {
-  console.log("prev response: ", event.prev.result);
-  return "SUCCESS";
+const AWS = require("aws-sdk");
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context, callback) => {
+  await createorder()
+    .then(() => {
+      callback(null, "SUCCESS");
+    })
+    .catch((err) => console.error(err));
 };
+
+function createorder() {
+  const params = {
+    TableName: "Order-dcjey6xckjddhcsd7yoy7mixqq-dev",
+    Item: {
+      id: "FFFFF",
+      user: "vs code test user",
+    },
+  };
+  return ddb.put(params).promise();
+}
